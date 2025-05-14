884649e489cdab1d483f098ca8b64e40996771fc
GuGauwu <gustavo.mayoral85@unach.mx>
2025-05-14 09:55:01 -0600

    Primer commit: pantallas angular con recursos

---
+++ src/app/nva-poliza/nva-poliza.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nva-poliza',
+  imports: [],
+  templateUrl: './nva-poliza.component.html',
+  styleUrl: './nva-poliza.component.scss'
+})
+export class NvaPolizaComponent {
+
+}
